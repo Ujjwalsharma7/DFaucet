6423a4eab505c8aa548782db10b8d49f0a69cac0
Ujjwal <ujjwalsharma1772003@gmail.com>
2023-09-12 18:56:59 +0530

    Initial commit.

---
+++ src/tokenprac_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { tokenprac_backend } from "../../declarations/tokenprac_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await tokenprac_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
