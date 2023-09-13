cc99188a5fa635214fc8f4b46663f5be861b0c9e
Ujjwal <ujjwalsharma1772003@gmail.com>
2023-09-14 04:08:42 +0530

    added backend and frontend to display symbol

--- src/tokenprac_frontend/src/components/Balance.jsx
+++ src/tokenprac_frontend/src/components/Balance.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { useState } from "react";
+import { tokenprac_backend } from "../../../declarations/tokenprac_backend/index";
+import { Principal } from "@dfinity/principal"
 function Balance() {
-  
+
+  const [inputValue, setInputValue] = useState("");
+  const [balanceResult, setBalance] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [hidden, setHidden] = useState(true);
+
   async function handleClick() {
-    console.log("Balance Button Clicked");
+    console.log(inputValue);
+
+    const principal = Principal.fromText(inputValue);
+    const balance = await tokenprac_backend.balanceOf(principal);
+    setBalance(balance.toLocaleString());
+    setSymbol(await tokenprac_backend.getSymbol());
+    setHidden(false);
   }
 
 
@@ -15,6 +28,8 @@ function Balance() {
           id="balance-principal-id"
           type="text"
           placeholder="Enter a Principal ID"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
       </p>
       <p className="trade-buttons">
@@ -25,7 +40,7 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of XYZ.</p>
+      <p hidden={hidden}>This account has a balance of {balanceResult} {symbol}.</p>
     </div>
   );
 }
